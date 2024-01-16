3a3f609832dadbffde573feee9f97bf5fd1789c6
Phyo Thu Ya <vik8312@gmail.com>
2024-01-16 09:44:18 -0800

    update

--- src/components/EmptyStages.jsx
+++ src/components/EmptyStages.jsx
@@ -5,7 +5,11 @@ import React from "react";
 const EmptyStages = () => {
 	return (
 		<div className="flex items-center justify-between align-middle  gap-5 ">
-			<img src="../../images/flame-2.png" className=" " alt="" />
+			<img
+				src="../../images/flame-2.png"
+				className=" "
+				alt=""
+			/>
 			<h2 className="text-xl  text-gray-600 w-full cursor-pointer ">
 				Ther is no record.Buy Something !
 			</h2>
