179bac6df0080ff6644c9937c1d40996ed7b4c2a
Phyo Thura <155300367+K2P4@users.noreply.github.com>
2024-01-16 06:13:08 -0800

    Update EmptyStages.jsx

--- src/components/EmptyStages.jsx
+++ src/components/EmptyStages.jsx
@@ -5,7 +5,7 @@ import React from "react";
 const EmptyStages = () => {
 	return (
 		<div className="flex items-center justify-between align-middle  gap-5 ">
-			<img src="../../images/flame-2.png" className=" " alt="" />
+			<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freepik.com%2Ffree-photos-vectors%2Fperson-asking%2F37&psig=AOvVaw3u3_GStxjBYBgD2tIIQ6QW&ust=1705500431386000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCNiOrdWr4oMDFQAAAAAdAAAAABAI" className=" " alt="" />
 			<h2 className="text-xl  text-gray-600 w-full cursor-pointer ">
 				Ther is no record.Buy Something !
 			</h2>
